bcf34f5ee8d4cd1271bda3b7a135f6fa5bb6fa91
SiskonEmilia <xiongyq7@mail2.sysu.edu.cn>
2018-12-15 19:43:44 +0800

    now able to read DS

---
+++ AI_Web/NumRecognition/model/dataset.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+import os
+import os.path
+
+'''
+This function is used to parse the DS file at the given path to get images.
+Paramater: 
+  - pathToDS: string path to indicate the file
+Return Value:
+  - pixels: array of type bytes, every byte is a pixel
+  - height: height of each image
+  - width:  width of each image 
+  - count:  number of images 
+'''
+def parseDSImage(pathToDS):
+  if not os.path.exists(pathToDS):
+    raise IOError
+  with open(pathToDS) as f:
+    f.read(4) # unused magic number
+
+    count = int.from_bytes(f.read(4), byteorder='big')
+    print('Parsing Image DS - Image count: ' + str(count))
+
+    height = int.from_bytes(f.read(4), byteorder='big')
+    print('Parsing Image DS - Image height: ' + str(height))
+    width = int.from_bytes(f.read(4), byteorder='big')
+    print('Parsing Image DS - Image width: ' + str(width))
+
+    pixels = []
+    for _ in range(count):
+      pixels.append(f.read(height * width))
+
+  return (pixels, height, width, count)
+
+'''
+This function is used to parse the DS file at the given path to get labels.
+Paramater: 
+  - pathToDS: string path to indicate the file
+Return Value:
+  - lables: array of type bytes, every byte is a pixel
+  - count:  number of labels
+'''
+def parseDSLabel(pathToDS):
+  if not os.path.exists(pathToDS):
+    raise IOError
+  with open(pathToDS) as f:
+    f.read(4) # unused magic number
+
+    count = int.from_bytes(f.read(4), byteorder='big')
+    print('Parsing Label DS - Label count: ' + str(count))
+
+    lables = []
+    for _ in range(count):
+      lables.append(f.read(1))
+
+  return (lables, count)
